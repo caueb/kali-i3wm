91f23bc0ac78ea70a1f069b19d4563b1aababae1
Caue Borella <caueborella@gmail.com>
2021-08-18 10:18:39 +0800

    Delete polybar directory

--- polybar/scripts/htb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-IFACE=$(ifconfig | grep tun0 | awk '{print $1}')
-
-if [ "$IFACE" = "tun0:" ]; then
-    sudo killall -q openvpn
-else
-    sudo openvpn /home/kali/Downloads/release_arena_cbx.ovpn
-fi
